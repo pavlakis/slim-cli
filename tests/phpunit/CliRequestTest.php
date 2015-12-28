26d44cfc8335b0ae7869b83d2a7357d7ef3c1f39
Antonis Pavlakis <antonis@pavlakis.info>
2015-12-28 16:44:28 +0000

    Added initial unit tests.

---
+++ tests/phpunit/CliRequestTest.php
@@ -0,0 +1,93 @@
+<?php
+namespace pavlakis\cli\tests;
+
+use pavlakis\cli\CliRequest;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Slim\Http\Body;
+use Slim\Http\Headers;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Http\Uri;
+
+class CliRequestTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+        global $argv;
+
+        $argv[0] = 'cli.php';
+        $argv[1] = '/status';
+        $argv[2] = 'GET';
+        $argv[3] = 'event=true';
+    }
+
+    /**
+     * Taken from: https://github.com/slimphp/Slim-HttpCache/blob/master/tests/CacheTest.php
+     * @return Request
+     */
+    public function requestFactory()
+    {
+        $uri = Uri::createFromString('https://example.com:443/foo/bar?abc=123');
+        $headers = new Headers();
+        $cookies = [];
+        $serverParams = [];
+        $body = new Body(fopen('php://temp', 'r+'));
+        return new Request('GET', $uri, $headers, $cookies, $serverParams, $body);
+    }
+
+    public function testCorrectRequestParametersArePassed()
+    {
+        $req = $this->requestFactory();
+        $res = new Response();
+        $next = function (Request $req, Response $res) {
+            return $res;
+        };
+
+        /** @var CliRequest $cliRequest */
+        $cliRequest = new CliRequest();
+
+        /** @var  ResponseInterface $res */
+        $res = $cliRequest($req, $res, $next);
+
+        $this->assertEquals('event=true', $cliRequest->getRequest()->getUri()->getQuery());
+    }
+
+    public function testRequestPathHasBeenUpdated()
+    {
+        $req = $this->requestFactory();
+        $res = new Response();
+        $next = function (Request $req, Response $res) {
+            return $res;
+        };
+
+        /** @var CliRequest $cliRequest */
+        $cliRequest = new CliRequest();
+
+        /** @var  ResponseInterface $res */
+        $res = $cliRequest($req, $res, $next);
+
+        $this->assertEquals('/status', $cliRequest->getRequest()->getUri()->getPath());
+    }
+
+    public function testRequestRemainsSameIfNoArgvIsPassed()
+    {
+        unset($GLOBALS['argv']);
+
+        $req = $this->requestFactory();
+        $res = new Response();
+        $next = function (Request $req, Response $res) {
+            return $res;
+        };
+
+        /** @var CliRequest $cliRequest */
+        $cliRequest = new CliRequest();
+
+        /** @var  ResponseInterface $res */
+        $res = $cliRequest($req, $res, $next);
+
+        $this->assertEquals($req, $cliRequest->getRequest());
+    }
+}
