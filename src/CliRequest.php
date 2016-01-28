9b3efd5af84304e527a33c9ee6b5844d627888a4
Antonis Pavlakis <antonis@pavlakis.info>
2016-01-28 23:05:08 +0000

    Added author credit for @bobbyjason where this code is based from.

--- src/CliRequest.php
+++ src/CliRequest.php
@@ -3,9 +3,10 @@
  * A Slim 3 middleware enabling a mock GET request to be made through the CLI.
  * Use in the form: php public/index.php /status GET event=true
  *
- * @link      https://github.com/pavlakis/slim-cli
- * @copyright Copyright © 2015-2016 Antonis Pavlakis
- * @license   https://github.com/pavlakis/slim-cli/blob/master/LICENSE (BSD 3-Clause License)
+ * @link        https://github.com/pavlakis/slim-cli
+ * @copyright   Copyright © 2015-2016 Antonis Pavlakis
+ * @author      Bobby DeVaux (@bobbyjason) Based on Bobby's code from: https://github.com/dvomedia/gulp-skeleton/blob/master/web/index.php
+ * @license     https://github.com/pavlakis/slim-cli/blob/master/LICENSE (BSD 3-Clause License)
  */
 namespace pavlakis\cli;
 
